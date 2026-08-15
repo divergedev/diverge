e4048b4a0dec52a479634be7c2d76ea986cd67a4
Bruce Arctor <5032356+brucearctor@users.noreply.github.com>
2026-08-15 13:25:07 -0700

    chore: remove race-prone ResetRegistry from database package

--- pkg/database/registry.go
+++ pkg/database/registry.go
@@ -4,8 +4,3 @@ import "github.com/divergedev/diverge/pkg/registry"
 
 // Providers is the registry of available DatabaseProvider implementations.
 var Providers = registry.New[DatabaseProvider]("database")
-
-// ResetRegistry clears all registered providers. For testing only.
-func ResetRegistry() {
-	Providers = registry.New[DatabaseProvider]("database")
-}
